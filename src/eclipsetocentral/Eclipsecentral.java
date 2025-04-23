a69709acb803b145b815aeed569a01b28b567262
ChaitanyaSaiKamal <kamalgurrala29@gmail.com>
2025-04-23 21:02:40 +0530

    commiting to central

---
+++ src/eclipsetocentral/Eclipsecentral.java
@@ -0,0 +1,14 @@
+package eclipsetocentral;
+
+public class Eclipsecentral {
+
+	public static void main(String[] args) 
+	{
+		int a=1,b=2;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+	}
+
+}
